bcee8773c617b6085990c1ae348ac65a59119e60
Yannick Willems <ywillems96@gmail.com>
2018-05-14 21:46:23 +0200

    Auto stash before merge of "studentenhuis" and "origin/studentenhuis"

---
+++ models/Meal.js
@@ -0,0 +1,11 @@
+class Meal {
+    constructor(name, description, ingredients, allergies, price){
+        this.name = name
+        this.description = description
+        this.ingredients = ingredients
+        this.allergies = allergies
+        this.price = price
+    }
+}
+
+module.exports = Meal
---
+++ models/StudentHouse.js
@@ -0,0 +1,8 @@
+class StudentHouse {
+    constructor(name, address){
+        this.name = name
+        this.address = address
+    }
+}
+
+module.exports = StudentHouse
